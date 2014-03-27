2e632e8767c320b8acafd79fe3e687ac7134e0df
JP Simard <jp@jpsim.com>
2014-03-27 16:19:15 -0400

    Fixed POST params and bumped presto_api to 0.0.6

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'presto_api', '0.0.5'
+gem 'presto_api', '0.0.6'
 gem 'rack-throttle'
 
 group :development do
--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ helpers do
 end
 
 before do
-	request.path_info = '/' unless request_headers['x_api_key'] == ENV['api_key']
+  if request.request_method == "POST"
+    body_parameters = request.body.read
+    params.merge!(JSON.parse(body_parameters))
+  end
+  request.path_info = '/' unless request_headers['x_api_key'] == ENV['api_key']
 end
 
 get '/' do
@@ -16,30 +20,30 @@ get '/' do
 end
 
 get '/card_status/:card_number' do
-	PrestoAPI::Client.new.card_status_with_number(params[:card_number]).to_json
+  PrestoAPI::Client.new.card_status_with_number(params[:card_number]).to_json
 end
 
 get '/card_status/:username/:password' do
-	PrestoAPI::Client.new.card_status_with_username_password(params[:username], params[:password]).to_json
+  PrestoAPI::Client.new.card_status_with_username_password(params[:username], params[:password]).to_json
 end
 
 get '/me/:username/:password' do
-	PrestoAPI::Client.new.user_with_username_password(params[:username], params[:password]).to_json
+  PrestoAPI::Client.new.user_with_username_password(params[:username], params[:password]).to_json
 end
 
 get '/transaction_history/:username/:password' do
-	PrestoAPI::Client.new.transaction_history_with_username_password(params[:username], params[:password]).map(&:to_json).to_json
+  PrestoAPI::Client.new.transaction_history_with_username_password(params[:username], params[:password]).map(&:to_json).to_json
 end
 
 post '/balance' do
-	credit_card = PrestoAPI::CreditCard.new
-	credit_card.name = params[:credit_card_name]
-	credit_card.number = params[:credit_card_number]
-	credit_card.expiry_month = params[:credit_card_expiry_month]
-	credit_card.expiry_year = params[:credit_card_expiry_year]
+  credit_card = PrestoAPI::CreditCard.new
+  credit_card.name = params[:credit_card_name]
+  credit_card.number = params[:credit_card_number]
+  credit_card.expiry_month = params[:credit_card_expiry_month]
+  credit_card.expiry_year = params[:credit_card_expiry_year]
 
-	puts PrestoAPI::Client.new.load_unregistered_card(params[:card_number],
-																										params[:email],
-																										params[:amount],
-																										credit_card).to_json
+  PrestoAPI::Client.new.load_unregistered_card(params[:card_number],
+                                               params[:email],
+                                               params[:amount],
+                                               credit_card).to_json
 end
